a560cbe09ab69f79372533bff55b54cc5c6dd386
KuprienkovV <paradoxum912@gmail.com>
2025-11-20 13:27:48 +0300

    [feat] added extended eslint config + packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-)
+// withNuxt() подтягивает конфиг из @nuxt/eslint-config:
+// там уже подключены Vue 3 + Nuxt правила, @typescript-eslint и связанные плагины.
+// Ниже мы лишь добавляем поверх точечные правила проекта.
+
+export default withNuxt({
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'nuxt/no-cjs-in-config': 'off'
+  }
+})
